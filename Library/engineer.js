6e3c8d2127890f85798ae2f7ec00dfb652057bed
Yah'Mir McGhee <yahmir.mcghee@gmail.com>
2022-02-26 12:35:09 -0500

    Resolving test issues

--- Library/engineer.js
+++ Library/engineer.js
@@ -3,14 +3,14 @@ const Employee = require("./employee");
 
 // This allows all info from employee to here: Part 2
 class Engineer extends Employee {
-    constructor(name, id, email, office) {
+    constructor(name, id, email, github) {
         // Below (super) is what we are grabbing/keeping from the original (employee.js). AKA Copy and paste 
         super(name,id, email);
         this.github = github; 
     }
     
 
-    getGithub () {  return this.getGithub;  }
+    getGithub () {  return this.github;  }
 
         getRole () {    return 'Engineer';  }
 }
